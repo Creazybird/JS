58828cf2bba6ae4135a2ab2b1c9d8482fde19fd2
creazybird <2633411459@qq.com>
2019-11-29 15:03:35 +0800

    add function

--- JS_Practice/JavaScript/demo2.js
+++ JS_Practice/JavaScript/demo2.js
@@ -13,6 +13,33 @@ Number.method('judgeZero',function(para){
 })
 
 var a=7
-
 var res=a.judgeZero()
 console.log(res)
+
+/*实现对Array添加一个reduce方法，它接收一个函数和一个初始值作为一个新值。
+它遍历这个数组，以当前元素和该初始值为参数调用这个函数，并且计算出一个新值。当
+完成时，它返回这个值。如果我们传入一个把两个数字相加的函数，它会计算出相加之和。
+如果我们传入两个数字相乘的函数，它会计算两者的乘积
+*/
+
+Array.method('reduce',function(f,value){
+    var i;
+    for(i=0;i<this.length;i+=1){
+        value=f(this[i],value)
+    }
+    return value;
+})
+
+var data=[4,8,15,16,23,42]
+var add=function(a,b){
+    return a+b;
+}
+var mult=function(a,b){
+    return a*b;
+}
+
+var sum=data.reduce(add,0);
+console.log(sum) //sum is 108
+
+var product=data.reduce(mult,1);
+console.log(product)
